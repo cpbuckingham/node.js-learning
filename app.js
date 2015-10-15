7837d48f22c0a3cd68e00505f0b992fefe299442
Cameron <cameron.p.buckingham@gmail.com>
2015-10-15 09:26:04 -0700

    js basics that helps with learning node js

--- app.js
+++ app.js
@@ -2,3 +2,22 @@
  * Created by cpbuckingham on 10/14/15.
  */
 console.log('bacon');
+
+var person = {
+    fn: "cam",
+    ln: "buckingham",
+    age: 28};
+console.log(person);
+
+function addNumber(a,b){
+return a + b;
+}
+console.log(addNumber(7,3));
+
+function worthless(){}
+console.log(worthless());
+
+var printBacon = function(){
+    console.log("bacon is healthy, don't believe the Doctors");
+}
+setTimeout(printBacon, 5000);
