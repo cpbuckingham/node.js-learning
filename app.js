1a89fecf86f6aa1001a3f27feaac15edd1a3bedf
Cameron <cameron.p.buckingham@gmail.com>
2015-10-16 08:55:52 -0700

    created modules

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-var cam = {
+var movies = require('./movies');
 
-    printFirstName: function(){
-        console.log("My name is cam");
-        console.log(this === cam);
-    }
-};
-
-cam.printFirstName();
-
-
-//the default calling context is global
-function doSomething(){
-    console.log("\nI am worthless");
-    console.log(this === global);
-
-}
-doSomething();
+console.log(movies);
