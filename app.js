f78eab4b298d5fb6f1b862ac58f867d39ed6cfa0
Cameron <cameron.p.buckingham@gmail.com>
2015-10-16 08:34:34 -0700

    implemented callbacks & setTimeout

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-/**
- * Created by cpbuckingham on 10/14/15.
- */
-console.log('bacon');
-
-var person = {
-    fn: "cam",
-    ln: "buckingham",
-    age: 28};
-console.log(person);
-
-function addNumber(a,b){
-return a + b;
+function placeOrder(orderNumber){
+    console.log("Customer order", orderNumber);
+    cookAndDeliverFood(function(){
+        console.log("Delivered food order", orderNumber);
+    });
+    
+}//simulate a 5 second operation
+function cookAndDeliverFood(callback){
+    setTimeout(callback, 5000);
 }
-console.log(addNumber(7,3));
 
-function worthless(){}
-console.log(worthless());
-
-var printBacon = function(){
-    console.log("bacon is healthy, don't believe the Doctors");
-}
-setTimeout(printBacon, 5000);
+//simulate users web request
+placeOrder(1);
+placeOrder(2);
+placeOrder(3);
+placeOrder(4);
+placeOrder(5);
